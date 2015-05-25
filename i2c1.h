c0fedd852f2de2bf22f8398f17a69cf75f829897
Josele <jl.moragas@hotmail.es>
2015-05-25 16:11:19 +0200

    Revert "Lsel2014"
    This reverts commit 244a656e114bcba3e8cbd467e9786478607e89a8.

--- i2c1.h
+++
@@ -1,6 +0,0 @@
-#ifndef I2C1_H
-#define I2C1_H
-
-void i2c_pins_setup(void);
-int i2c_cmd(char* arg);
-#endif
--- sunTasks.h
+++
@@ -1,8 +0,0 @@
-#ifndef SUN_TASKS_H
-#define SUN_TASKS_H
-
-#define TASK_SUN_PERIOD 2000000000
-
-void daemon_update_sun(void *arg);
-
-#endif
