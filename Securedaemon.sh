f9163e06c296ed63b82085dfac1990c0f47b7d36
Josele <jl.moragas@hotmail.es>
2015-11-04 10:00:55 +0000

    changed port

---
+++ Securedaemon.sh
@@ -0,0 +1,10 @@
+#!/bin/sh 
+echo start
+sudo fuser -k 45000/tcp;
+sudo fuser -k 3000/tcp;
+
+sudo ./daemon & ../node-v0.10.28-linux-arm-pi/bin/node ../Raspserver/bin/www 
+
+gpio write 19 0
+gpio write 20 0
+
