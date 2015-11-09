34caee8025d1df308fa2f623f079d588d14ba5b5
Josele <jl.moragas@hotmail.es>
2015-11-09 23:57:26 +0000

    Run the c program at the same time as the node server

--- Securedaemon.sh
+++ Securedaemon.sh
@@ -2,8 +2,9 @@
 echo start
 sudo fuser -k 45000/tcp;
 sudo fuser -k 3000/tcp;
+sudo ./daemon &
+../node-v0.10.28-linux-arm-pi/bin/node ../Raspserver/bin/www ;
 
-../node-v0.10.28-linux-arm-pi/bin/node ../Raspserver/bin/www & sudo ./daemon 
 
 gpio write 19 0
 gpio write 20 0
