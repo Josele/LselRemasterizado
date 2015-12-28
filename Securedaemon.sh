ea1eb83a109fdb192d3d7b810c2fdd632c5dd434
Josele <jl.moragas@hotmail.es>
2015-12-28 17:30:53 +0000

    socket real unix

--- Securedaemon.sh
+++ Securedaemon.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 echo start
-sudo fuser -k 45000/tcp;
+sudo rm socket;
 sudo fuser -k 3000/tcp;
 sudo ./daemon 2> treneserrores.log &
 sudo ../node-v0.10.28-linux-arm-pi/bin/node ../Raspserver/bin/www;
